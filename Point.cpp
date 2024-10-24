2675d1486be94685e44e0a5061be0764d8dfe1c4
bryates-eecs <bryates@umich.edu>
2024-10-24 14:59:05 -0400

    Completed_Point.cpp

--- Point.cpp
+++ Point.cpp
@@ -5,10 +5,10 @@
  * EECS 183
  * Project 4: CoolPics
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * Blake Yates, Amelia Wu
+ * bryates, wuamelia
  *
- * <#Description#>
+ * Definitions for class Point constructors and member fu
  */
 
 #include "Point.h"
@@ -16,8 +16,60 @@
 // for the declaration of DIMENSION
 #include "utility.h"
 
-// TODO: implement two constructors, setX, getX, setY, getY, read, write, checkRange.
+int Point::checkRange(int val) {
+    if (val >= 0 && val < DIMENSION) {
+        return val;
+    }
+    else if (val < 0) {
+        return 0;
+    }
+    else {
+        return (DIMENSION - 1);
+    }
+}
 
+Point::Point() {
+    x = 0;
+    y = 0;
+}
+
+Point::Point(int inX, int inY) {
+    x = checkRange(inX);
+    y = checkRange(inY);
+}
+
+
+void Point::setX(int inX) {
+    x = checkRange(inX);
+    return;
+}
+
+int Point::getX() {
+    return x;
+}
+
+void Point::setY(int inY) {
+    y = checkRange(inY);
+    return;
+}
+
+int Point::getY() {
+    return y;
+}
+
+void Point::read(istream& ins) {
+    char junk = '/';
+    ins >> junk >> x >> junk >> y >> junk;
+    x = checkRange(x);
+    y = checkRange(y);
+
+    return;
+}
+
+void Point::write(ostream& outs) {
+    outs << "(" << x << "," << y << ")";
+    return;
+}
 
 
 // Your code goes above this line.
