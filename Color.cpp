4b35bcd0488fd1a39835ed0427b4a493f274808a
bryates-eecs <bryates@umich.edu>
2024-10-24 14:55:52 -0400

    Stubbed_Color.cpp

--- Color.cpp
+++ Color.cpp
@@ -5,10 +5,10 @@
  * EECS 183
  * Project 4: CoolPics
  *
- * <#Name(s)#>
- * <#uniqname(s)#>
+ * Blake Yates, Amelia Wu
+ * bryates, wuamelia
  *
- * <#Description#>
+ * Definitions for class Color constructors and member fu
  */
 
 #include "Color.h"
@@ -16,6 +16,62 @@
 // TODO: implement first checkRange, then two constructors, setRed, getRed,
 //       setGreen, getGreen, setBlue, getBlue, read, write.
 
+int checkRange(int val) {
+	//to do- implement
+
+	//to do- replace with correct return statement
+	return 0;
+}
+
+//default constructor
+Color::Color() {
+	//to do- implement
+}
+
+Color::Color(int redVal, int greenVal, int blueVal) {
+	//to do- implement
+}
+
+void Color::setRed(int redVal) {
+	//to do- implement
+}
+
+int Color::getRed() {
+	//to do- implement
+
+	//to do- replace with correct return statement
+	return 0;
+}
+
+void Color::setGreen(int greenVal) {
+	//to do- implement
+}
+
+int Color::getGreen() {
+	//to do- implement
+
+	//to do- replace with correct return statement
+	return 0;
+}
+
+void Color:: setBlue(int blueVal) {
+	//to do- implement
+}
+
+int Color::getBlue() {
+	//to do- implement
+
+	//to do- replace with correct return statement
+	return 0;
+}
+
+void Color::read(istream& ins) {
+	//to do- implement
+}
+
+void Color::write(ostream& outs) {
+	//to do- implement
+}
 
 
 // Your code goes above this line.
@@ -32,3 +88,4 @@ ostream& operator << (ostream& outs, Color color)
    color.write(outs);
    return outs;
 }
+
