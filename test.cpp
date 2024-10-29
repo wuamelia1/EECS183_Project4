23235d84f19953dd69e0e95784d8a0bc230a4755
wuamelia1 <wuamelia@umich.edu>
2024-10-29 12:18:31 -0400

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -33,20 +33,14 @@ void test_Line();
 void test_Point();
 void test_Rectangle();
 void test_Triangle();
-void test_Graphics();
-void file_check();
 
 void startTests() {
     test_Point();
     test_Color();
-    test_Graphics();
     test_Line();
     test_Triangle();
-    file_check();
     test_Circle();
     test_Rectangle();
-    // call other test functions here
-
     return;
 }
 
@@ -109,32 +103,20 @@ void test_Color() {
     return;
 }
 
-void test_Graphics() {
-    cout << endl << "Now testing class Graphics" << endl;
-    Color c1(0, 0, 0);
-    //test of default constructor
-    Graphics graphic1;
-    graphic1.writeFile("TextFile1.txt");
-    //test of non-default constructor
-    graphic1.setPixel(0, 0, c1);
-
-    //
-    return;
-}
 
 void test_Line() {
     cout << endl << "Now testing class Line" << endl;
 
     //Default constructor test
     Line line1;
-    cout << "Expected: (0,0) (0,0) 0 0 0, Actual: " << line1 << endl;
+    cout << "Expected: (0,0)  (0,0)  0  0  0, Actual: " << line1 << endl;
 
     //Non-default constructor test
     Point pt1(0, 0);
     Point pt2(0, 5);
     Color c1;
     Line line2(pt1, pt2, c1);
-    cout << "Expected: (0,0) (0,5) 0 0 0, Actual: " << line2 << endl;
+    cout << "Expected: (0,0)  (0,5)  0  0  0, Actual: " << line2 << endl;
 
     //test of function set start and get start
     line1.setStart(pt2);
@@ -143,18 +125,19 @@ void test_Line() {
     //test of function set end and get end
     Point p3(6, 7);
     line2.setEnd(p3);
-    cout << "Expected: (6, 7), Actual : " << line2.getEnd() << endl;
+    cout << "Expected: (6,7), Actual : " << line2.getEnd() << endl;
 
     //test of function set color and get color
     Color c2(3, 4, 5);
     line1.setColor(c2);
-    cout << "Expected: 3 4 5, Actual: " << line1.getColor() << endl;
+    cout << "Expected: 3  4  5, Actual: " << line1.getColor() << endl;
 
-    //test write
-    ofstream output_file;
-    output_file.open("data2.txt");
-    line2.write(output_file);
-
+    //test of function write
+    Line line3(pt1, pt2, c1);
+    fstream input_file;
+    input_file.open("data1.txt");
+    cout << "Expected: (0,0)  (0,5)  0  0  0, Actual: " << endl;
+    line3.write(input_file);
 }
 
 void test_Triangle() {
@@ -167,15 +150,17 @@ void test_Triangle() {
     Color c1(1, 2, 3);
     Color c2(0, 0, 0);
     Color c3(4, 5, 5);
+    Color c4(-5, 0, 0);
+    Color c5(7, 256, 8);
 
     //test of default constructor
     Triangle t1;
-    cout << "Expected: (0,0) 0 0 0 (0,0) 0 0 0 (0,0) 0 0 0, Actual: " <<
+    cout << "Expected: (0,0)  0  0  0  (0,0)  0  0  0  (0,0)  0  0  0, Actual: " <<
         t1 << endl;
 
     //test of non-default constructor
     Triangle t2(p1, c1, p2, c2, p3, c3);
-    cout << "Expected: (0,0) 1 2 3 (1,5) 0 0 0 (6,7) 4 5 5, Actual: " <<
+    cout << "Expected: (0,0)  1  2  3  (1,5)  0  0  0  (6,7)  4  5  5, Actual: " <<
         t2 << endl;
 
     //test of function getVertexOne() and setVertexOne()
@@ -184,7 +169,9 @@ void test_Triangle() {
 
     //test of function getVertexOneColor() and setVertexOneColor()
     t2.setVertexOneColor(c2);
-    cout << "Expected: 0 0 0, Actual: " << t2.getVertexOneColor() << endl;
+    cout << "Expected: 0  0  0, Actual: " << t2.getVertexOneColor() << endl;
+    t2.setVertexOneColor(c1);
+    cout << "Expected: 1  2  3, Actual: " << t2.getVertexOneColor() << endl;
 
     //test of function getVertexTwo() and setVertexTwo()
     t2.setVertexTwo(p3);
@@ -192,7 +179,9 @@ void test_Triangle() {
 
     //test of function getVertexTwoColor() and setVertexTwoColor()
     t2.setVertexTwoColor(c1);
-    cout << "Expected: 1 2 3, Actual: " << t2.getVertexTwoColor() << endl;
+    cout << "Expected: 1  2  3, Actual: " << t2.getVertexTwoColor() << endl;
+    t2.setVertexTwoColor(c4);
+    cout << "Expected: 0  0  0, Actual: : " << t2.getVertexTwoColor() << endl;
 
     //test of function getVertexThree() and setVertexThree()
     t2.setVertexThree(p1);
@@ -200,7 +189,14 @@ void test_Triangle() {
 
     //test of function getVertexThreeColor() and setVertexThreeColor()
     t2.setVertexThreeColor(c1);
-    cout << "Expected: 1 2 3, Actual: " << t2.getVertexThreeColor() << endl;
+    cout << "Expected: 1  2  3, Actual: " << t2.getVertexThreeColor() << endl;
+    t2.setVertexThreeColor(c5);
+    cout << "Expected: (7, 255, 8), Actual: " << t2.getVertexThreeColor() << endl;
+
+    //test of function setColor()
+    t1.setColor(c1);
+    cout << "Expected: (0,0)  1  2  3  (0,0)  1  2  3  (0,0)  1  2  3, Actual: " <<
+        t1 << endl;
 }
 
 void test_Circle() {
@@ -208,17 +204,16 @@ void test_Circle() {
 
     //test of default constructor
     Circle c1;
-    cout << "Expected: (0,0) 0 0  0  0, actual: " << c1 << endl;
+    cout << "Expected: (0,0)  0  0  0  0, actual: " << c1 << endl;
 
-    //test of non-default constructor (and checkRadius)
-    Point initCenter(-200, 700);
-    Point initCenter2(30, 40);
+    //test of non-default constructor
+    Point initCenter(10, 71);
     Color red(255, 0, 0);
-
     Circle c2(initCenter, 4, red);
-    Circle c3(initCenter2, -1, red);
-    cout << "Expected: (0,71) 4 255  0  0, actual: " << c2 << endl;
-    cout << "Expected: (30,40) 1 255  0  0, actual: " << c3 << endl;
+    cout << "Expected: (10,71) 4 255  0  0, actual: " << c2 << endl;
+    Circle c3(initCenter, -4, red);
+    cout << "Expected: (10,71) 4 255  0  0, actual: " << c2 << endl;
+
 
     //test member functions set and get center
     Point newCenter(12,3);
@@ -228,32 +223,13 @@ void test_Circle() {
     //test member functions set and get radius
     c1.setRadius(74);
     cout << "Expected: 74, actual: " << c1.getRadius() << endl;
-    c1.setRadius(-30);
-    cout << "Expected: 30, actual: " << c1.getRadius() << endl;
+    c1.setRadius(-5);
+    cout << "Expected: 5, actual: " << c1.getRadius() << endl;
 
     //test member functions set and get color
     Color blue(0, 0, 255);
     c1.setColor(blue);
     cout << "Expected: 0  0  255, actual: " << c1.getColor() << endl;
-}
-
-
-void file_check() {
-
-    // open one of the text files that are part of the starter code
-    ifstream ins;
-    ins.open("TextFile1");
-
-    // if the file was not in the correct directory, the stream state is fail
-    if (ins.fail()) {
-        cout << "Could not open TextFile1" << endl;
-    }
-    else {
-        cout << "Success! Text files are in the right directory." << endl;
-    }
-    ins.close();
-
-    return;
 }
 
 void test_Rectangle() {
@@ -276,18 +252,15 @@ void test_Rectangle() {
     Rectangle r2(p1, p2, c1, c2, c3, c4);
     cout << "Expected (10,10)  (20,30)  0  1  2  3  4  5  10  10  10  5  6  7, Actual: " << r2 << endl;
 
-    //test of function setColor
-    Color red(255, 0, 0);
-    r1.setColor(red);
-    cout << "Expected: (0,0) (0,0) 255  0  0 255  0  0 255  0  0 0  0  0, Actual: " << r1 << endl;
-
     //test of function setStart and getStart
     r1.setStart(p3);
     cout << "Expected: (9,9), Actual: " << r1.getStart() << endl;
 
     //test of function setEnd and getEnd
     r1.setEnd(p3);
-    cout << "Expected: (9,9), Actual: " << r2.getEnd() << endl;
+    cout << "Expected: (9,9), Actual: " << r1.getEnd() << endl;
+    r1.setEnd(p1);
+    cout << "Expected: (10,10), Actual: " << r1.getEnd() << endl;
 
     //test of function setColorTopLeft and getColorTopLeft
     r1.setColorTopLeft(c5);
@@ -304,6 +277,10 @@ void test_Rectangle() {
     //test of function setColorBottomLeft and getColorBottomLeft
     r1.setColorBottomLeft(c5);
     cout << "Expected 2  3  4, Actual: " << r1.getColorBottomLeft() << endl;
+
+    //test of function setColor
+    r2.setColor(c1);
+    cout << "Expected (10,10)  (20,30)  0  1  2  0  1  2  0  1  2  0  1  2, Actual: " << r2 << endl;
 
     return;
 }
